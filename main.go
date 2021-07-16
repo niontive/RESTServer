241cda5d8f7aafdecf74463bb710c660037ca580
niontive <niontive@microsoft.com>
2021-07-16 13:20:19 -0700

    Use const for port number

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 )
 
+const port = "10000"
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
@@ -13,7 +15,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":10000", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func main() {
