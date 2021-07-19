7e11be0a592648d11573f3ecfe323040c311d0c6
niontive <niontive@microsoft.com>
2021-07-18 23:33:04 -0700

    Add skeleton function for POST AppMetaData request

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -9,6 +10,13 @@ import (
 )
 
 const port = ":10000"
+
+func createNewAppMetaData(w http.ResponseWriter, r *http.Request) {
+	// get the body of our POST request
+	// return the string response containing the request body
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	fmt.Println(string(reqBody))
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -18,6 +26,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/appmetadata", createNewAppMetaData).Methods("POST")
 	log.Fatal(http.ListenAndServe(port, myRouter))
 }
 
