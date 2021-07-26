bc4528be60969dc8d128a2321a441aae5b6bb5d5
niontive <niontive@microsoft.com>
2021-07-26 12:09:52 -0700

    Add build, unit test, and clean build flags

--- build.sh
+++ build.sh
@@ -2,4 +2,24 @@
 
 export DOCKER_BUILDKIT=1
 
-docker build --target bin --output bin/ .
+build_server() {
+    docker build --target bin --output bin/ .
+}
+
+run_unit_tests() {
+    docker build --progress=plain --target unit-test .
+}
+
+clean() {
+    rm -rf ./bin
+    docker builder prune -f
+}
+
+while getopts "btc" flag
+do
+    case "${flag}" in
+        b) build_server;;
+        t) run_unit_tests;;
+        c) clean;;
+    esac
+done
