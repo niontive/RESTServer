3ffcf9cb781fd8622f7fa4744f55536aea9d01fe
niontive <niontive@microsoft.com>
2021-07-26 20:25:33 -0700

    Add ratelimit test

---
+++ test/ratelimit/curl_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Start RESTFul Server
+start_server() {
+    # TODO: make this configurable
+    ../../bin/server
+}
+
+# POST metadata
+post_metadata() {
+    curl -i -X POST localhost:10000/createmetadata \
+         -H "Content-Type: text/x-yaml" \
+         --data-binary @"$1"
+}
+
+start_server &
+SERVER_PID=$!
+
+for i in {0..100}
+    do
+        post_metadata "../yaml/valid1.yaml"
+
+done
+
+kill $SERVER_PID
