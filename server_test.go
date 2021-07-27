5f631494bb003db7390040d3281af14afdda9910
niontive <niontive@microsoft.com>
2021-07-26 21:30:35 -0700

    Add unit tests for ValidateAppMetaData

--- server_test.go
+++ server_test.go
@@ -22,3 +22,34 @@ func TestReplaceUnderscore(t *testing.T) {
 		t.Errorf("replaceUnderscore str = '%v'; want 'hello_world'", testSlice[0])
 	}
 }
+
+func TestValidateAppMetaData(t *testing.T) {
+	var data appMetaData
+	var err error
+
+	data.Title = "Test Title"
+	data.Version = "1"
+	data.Maintainers = append(data.Maintainers, maintainer{Name: "Test Name", Email: "test@test.com"})
+	data.Company = "Test Company"
+	data.Website = "https://www.test.com"
+	data.Source = "https://github.com/test"
+	data.License = "TestLicense"
+	data.Description = "Test Description"
+
+	err = validateAppMetaData(data)
+	if err != nil {
+		t.Errorf("Error validating ok metadata: %v", err)
+	}
+
+	data.Website = "test"
+	err = validateAppMetaData(data)
+	if err == nil {
+		t.Errorf("Error validating incorrect metadata: website field")
+	}
+
+	data = appMetaData{}
+	err = validateAppMetaData(data)
+	if err == nil {
+		t.Errorf("Error validting incorrect metadata: empty struct")
+	}
+}
