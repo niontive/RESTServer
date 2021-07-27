60f0d69c6a9d6b5fb193b563c7385179d5056c24
niontive <niontive@microsoft.com>
2021-07-26 21:44:33 -0700

    Add unit test for AppMetaDataStore

--- server_test.go
+++ server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -44,12 +45,51 @@ func TestValidateAppMetaData(t *testing.T) {
 	data.Website = "test"
 	err = validateAppMetaData(data)
 	if err == nil {
-		t.Errorf("Error validating incorrect metadata: website field")
+		t.Error("Error validating incorrect metadata: website field")
 	}
 
 	data = appMetaData{}
 	err = validateAppMetaData(data)
 	if err == nil {
-		t.Errorf("Error validting incorrect metadata: empty struct")
+		t.Error("Error validting incorrect metadata: empty struct")
+	}
+}
+
+func TestAppMetaDataStore(t *testing.T) {
+	var dataStore = appMetaDataStore{store: make([]appMetaData, 0), dupTracker: make(map[string]bool)}
+	var data appMetaData
+	var err error
+
+	data.Title = "Test Title"
+	data.Version = "1"
+	data.Maintainers = append(data.Maintainers, maintainer{Name: "Test Name", Email: "test@test.com"})
+	data.Company = "Test Company"
+	data.Website = "https://www.test.com"
+	data.Source = "https://github.com/test"
+	data.License = "TestLicense"
+	data.Description = "Test Description"
+
+	err = dataStore.Add(data)
+	if err != nil {
+		t.Errorf("Unable to add MD to data store: %v", err)
+	}
+
+	err = dataStore.Add(data)
+	if err == nil {
+		t.Error("Added duplicate metadata to data store")
+	}
+
+	if dataStore.TotalEntries() != 1 {
+		t.Error("Incorrect number of data store entries")
+	}
+
+	searchMD := make([]string, 1)
+	searchMD = append(searchMD, "Test Title")
+	retrievedMD, err := dataStore.Search("title", searchMD)
+	if err != nil {
+		t.Errorf("Error in data store search: %v", err)
+	}
+	if !reflect.DeepEqual(retrievedMD[0], data) {
+		t.Error("Retrieved MD does not match added MD")
 	}
 }
