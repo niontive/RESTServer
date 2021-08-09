38b4f4a377dffec3657aa56a2248d2aa052d8baa
niontive <niontive@microsoft.com>
2021-08-09 07:17:58 -0700

    Cleanup context code for create

--- create.go
+++ create.go
@@ -8,62 +8,78 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-//
-// Add new metadata entry to metadata store
-//
-func doCreateNewAppMetaData(ctx context.Context, r *http.Request) error {
-	c := make(chan error) // Channel for anonymous function
-	go func(ctx context.Context) {
-		logger.Info("Endpoint Hit: createNewAppMetaData")
+func dataStoreAdd(ctx context.Context, ch chan error, data appMetaData) {
+	logger.Info("Adding to datastore")
+	if ctx.Err() != nil {
+		ch <- ctx.Err()
+		return
+	}
+	err := dataStore.Add(data)
+	logger.Info("Finished adding to datastore")
+	ch <- err
+}
 
-		// Read request into metadata structure
-		reqBody, _ := ioutil.ReadAll(r.Body)
-		data := appMetaData{}
-		errFunc := yaml.Unmarshal(reqBody, &data)
-		if errFunc != nil {
-			logger.Warnf("Unable to unmarshall POST request: %v", errFunc)
-			goto done
-		}
-		logger.Info("Unmarshalled POST request")
+//
+// Add metadata entry to datastore
+//
+func doAddAppMetaData(ctx context.Context, data appMetaData) error {
+	ch := make(chan error)
 
-		// Validate metadata and add to metadata store
-		errFunc = validateAppMetaData(data)
-		if errFunc != nil {
-			logger.Warnf("Failed to validate app metadata: %v", errFunc)
-			goto done
-		}
-		select {
-		case <-ctx.Done():
-			errFunc = ctx.Err() // Context is done so don't perform more operations
-			goto done
-		default:
-		}
-		errFunc = dataStore.Add(data)
-		if errFunc != nil {
-			logger.Warnf("Failed to add app metadata: %v", errFunc)
-			goto done
-		}
-		logger.Infof("Stored metadata for app '%v'", data.Title)
-		logger.Infof("App metadata store contains %v entries", dataStore.TotalEntries())
-	done:
-		c <- errFunc
-	}(ctx)
+	go dataStoreAdd(ctx, ch, data)
+
 	select {
-	case <-ctx.Done(): // End operation if ctx.Done() occurs
+	case <-ctx.Done():
+		logger.Warn("Request context is closed")
 		return ctx.Err()
-	case err := <-c:
+	case err := <-ch:
 		return err
 	}
+}
+
+//
+// Create and add new metadata entry via user request
+//
+func doCreateNewAppMetaData(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	var err error
+	logger.Info("Endpoint Hit: createNewAppMetaData")
+
+	// Read and validate request into metadata object
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	data := appMetaData{}
+	err = yaml.Unmarshal(reqBody, &data)
+	if err != nil {
+		logger.Warnf("Unable to unmarshall POST request: %v", err)
+		goto done
+	}
+	logger.Info("Unmarshalled POST request")
+
+	err = validateAppMetaData(data)
+	if err != nil {
+		logger.Warnf("Failed to validate app metadata: %v", err)
+		goto done
+	}
+
+	// Add entry to datastore
+	err = doAddAppMetaData(ctx, data)
+	if err != nil {
+		logger.Warnf("Failed to add app metadata: %v", err)
+		goto done
+	}
+	logger.Infof("Stored metadata for app '%v'", data.Title)
+	logger.Infof("App metadata store contains %v entries", dataStore.TotalEntries())
+
+done:
+	if err != nil {
+		internalError := http.StatusInternalServerError
+		http.Error(w, err.Error(), internalError)
+	}
+	return
 }
 
 //
 // Handler for creating new metadata entry
 //
 func createNewAppMetaData(w http.ResponseWriter, r *http.Request) {
-	err := doCreateNewAppMetaData(r.Context(), r)
-	if err != nil {
-		logger.Warnf("createNewAppMetaData error: %v", err)
-		internalError := http.StatusInternalServerError
-		http.Error(w, err.Error(), internalError)
-	}
+	ctx := r.Context()
+	doCreateNewAppMetaData(ctx, w, r)
 }
